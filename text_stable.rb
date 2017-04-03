b3142e8a8dee215bd7e4965b877f1c29dab5ac11
ahigley <addison.higley@gmail.com>
2017-04-03 13:00:58 -0500

    Bootstrap
    An attempt at boot strapping. Some changes to items as well.
    Bootstrapping only works for text_exp.rb. After some testing I'll update
    text.rb (essentially the stable version of the game)

--- text_stable.rb
+++ text_stable.rb
@@ -1,4 +1,6 @@
-
+puts "This game is still under development. For questions or help contact me at addison.higley@gmail.com"
+puts "If you are here because I applied for an internship, please hire me. This game is basic but I'm learning quickly."
+puts "I would rather be building something worth some money than this."
 
 class Items
 	MASTERLIST = [
@@ -82,7 +84,122 @@ class Game
 					
 	#Since the Game class is essentially the game engine, most user actions are still here but there is a lot of room for rebuilding. Below you can see we're calling a method within $hero.
 	#It's probably going to have a separate class full of user actions alone at some point but for now, it is what it is.
-		def get(thing)
+		def get(thing)def move_disambig(direction)
+			@north_cmds = ["n", "up", "north"]
+			@east_cmds = ["e", "right", "east"]
+			@west_cmds = ["w", "left", "west"]
+			@south_cmds = ["s", "down", "south"]
+
+					xy = $hero.location.xy.split(", ")
+					x = xy[0].to_i
+					y = xy[1].to_i
+				if @north_cmds.include?(direction)
+					if $hero.location.exits.include?("north")
+						check_room("north", x, (y + 1))
+						move("north")
+					else
+						puts "There is no exit to the north!"
+					end
+				elsif @east_cmds.include?(direction)
+					if $hero.location.exits.include?("east")
+						check_room("east", (x + 1), y)
+						move("east")
+					else
+						puts "There is no exit to the east!"
+					end
+				elsif @west_cmds.include?(direction)
+					if $hero.location.exits.include?("west")
+						check_room("west", (x - 1), y)
+						move("west")
+					else
+						puts "There is no exit to the west!"
+					end
+				elsif @south_cmds.include?(direction)
+					if $hero.location.exits.include?("south")
+						check_room("south", x, (y - 1))
+						move("south")
+					else 
+						puts "There is no exit to the south!"
+					end
+				end
+		end
+		def move(direction)
+				newroom = $hero.location.exits[direction]
+				puts "move"
+				p newroom
+				$hero.location = newroom
+				$hero.location.xy = newroom.xy
+		end
+
+	def check_room(direction, x, y)
+			xy_pos = "#{x}, #{y}"	
+		if @@all_rooms.has_key?(xy_pos) && (@@all_rooms[xy_pos] != nil)
+			check_exits(x, y)
+		else
+			generate(direction, x, y)
+		end
+	end
+
+
+	def check_exits(x, y)
+		n = y+1
+		s = y-1
+		e = x+1
+		w = x-1
+		north = "#{x}, #{n}"
+		south = "#{x}, #{s}"
+		east = "#{e}, #{y}"
+		west = "#{w}, #{y}"
+		room = @@all_rooms["#{x}, #{y}"]
+		if @@all_rooms.has_key?(east) && (@@all_rooms[east] != nil)
+			room.exits.merge!({"east" => @@all_rooms[east]})
+		elsif @@all_rooms.has_key?([west]) && (@@all_rooms[west] != nil)
+			room.exits.merge!({"west" => @@all_rooms[west]})
+		elsif @@all_rooms.has_key?([north]) && (@@all_rooms[north] != nil)
+			room.exits.merge!({"north" => @@all_rooms[north]})
+		elsif @@all_rooms.has_key?([south]) && (@@all_rooms[south] != nil)
+			room.exits.merge!({"south" => @@all_rooms[south]})
+		end
+	end
+	
+	def generate(direction, x, y)
+		@room = Rooms.new
+		@room.desc_gen
+		@room.item_gen
+		@room.xy = "#{x}, #{y}"
+		new_room = { @room.xy => @room}
+		@@all_rooms.merge!(new_room)
+		check_exits(x,y)
+		if direction == "north"
+			new_north = {"south" => $hero.location}
+			@room.exits.merge!(new_north)
+			north = {"north" => @room}
+			$hero.location.exits.merge!(north)
+		elsif direction == "south"
+			new_south = {"north" => $hero.location}
+			@room.exits.merge!(new_south)
+			south = {"south" => @room}
+			$hero.location.exits.merge!(south)
+		elsif direction == "east"
+			new_east = {"west" => $hero.location}
+			@room.exits.merge!(new_east)
+			east = {"east" => @room}
+			$hero.location.exits.merge!(east)
+		elsif direction == "west"
+			new_west = {"east" => $hero.location}
+			@room.exits.merge!(new_west)
+			west = {"west" => @room}
+			$hero.location.exits.merge!(west)
+		end
+		check_exits(x, y)
+		possible = (["north", "south", "east", "west"] - @room.exits.keys)
+		(rand(possible.count)).times do |gen|
+			actual = (possible - @room.exits.keys)
+			gen = actual.sample
+			@room.exits.merge!({gen => nil})
+		end
+	end
+
 			$hero.pick_up(thing)
 		end
 
@@ -235,7 +352,8 @@ class Game
 	#to say dump pot has a word2 value of pot but we can only evaluate it within the context of drop_cmds. At present its more orderly to have everything in one place. In future calling seperate methods such as drop_cmds_disambig
 	#might be a way of branching with maintaining readability.
 	def word_disambig(word1, word2, word3) 
-			go_cmds = ["go", "walk", "run", "move", "travel", "skip", "g", "m"]
+			go_cmds = ["go",  "walk", "run", "move", "travel", "skip", "g", "m"]
+			fast_move_cmds = ["n", "s", "e", "w"]
 			look_cmds = ["look", "see", "l"]
 			get_cmds = ["grab", "get", "take"]
 			drop_cmds = ["drop", "leave", "dump"]
@@ -247,6 +365,8 @@ class Game
 			@all_cmds << (look_cmds + get_cmds + drop_cmds)
 			if go_cmds.include?(word1)
 				move_disambig(word2)
+			elsif fast_move_cmds.include?(word1)
+				move_disambig(word1)
 			elsif look_cmds.include?(word1)
 					puts "
 					"
---
+++ text_world.rb
@@ -0,0 +1,64 @@
+#World class is nearly entirely scruft at this point. Orginally it held the procedural generation alrorithm. The decision to draw rooms as they were moved to meant that the algorithm had to be moved
+#into the Game class.
+class World < Game
+	attr_accessor :all_rooms, :exits_possible
+	def initialize
+		
+			end
+	
+end		
+class Rooms < World
+	attr_accessor :desc, :exits, :items, :xy
+	# @@room_count currently serves no purpose, but its long term purpose to set score/mob difficulty. Additionally if we want a 'stairs down' option after a certain number of rooms like one might find in diablo or pixel dungeon
+	# then generating stairs after a certain number of rooms could also be possible. Currently this is not implemented into the procedural generation algorithm.
+	@@room_count = 0
+	def initialize
+		@@room_count += 1
+		@exits = Hash.new
+		@items = Array.new
+		@xy = String.new
+	end
+		
+
+	
+	#Needs to be rewritten to contain an array of description types. Then array.sample could be used to pick a description. Moreover duplicating certain description types would offer a method of 
+	#weighting which types of rooms generated -- luxurious treasure rooms should occur less often than barren rooms.	
+	def desc_gen
+		choice = rand(1..3)
+		case choice
+		when 1
+			@desc = "You see... the first type of room!"
+		when 2
+			@desc = "You see... the second type of room!"
+		when 3
+			@desc = "You see... the third type of room!"
+		end
+	end
+	#Currently item_gen is in no way connected to desc_gen -- this needs to be changed. desc_gen only has place holder generation descrsiptions at the moment. In future meaninful descriptions should pair with some sort of loot
+	#table. If the description generated is a treasure room, the chance of loot should be higher. Additionally an alchemists chamber should have a higher chance of generating potions. Presently this isn't represented in item_gen
+	def item_gen
+		number = rand(3)
+		case number
+		when 1
+			@loot1 = Items.new
+			@items = [@loot1]	
+		when 2
+			@loot1 = Items.new
+			@loot2 = Items.new
+			@items = [@loot1, @loot2]
+		when 3
+			@loot1 = Items.new
+			@loot2 = Items.new
+			@loot3 = Items.new
+			@items = [@loot1, @loot2, @loot3]
+		else
+			@loot1 = Items.new
+			@loot1.desc = "There are no items here."
+			@items = [@loot1]
+		end
+	end
+
+
+
+
+end
